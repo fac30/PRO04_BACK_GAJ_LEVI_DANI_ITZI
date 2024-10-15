c43eba998159918b4470bd243478db7c31b3e2a5
rogers7levi <rogers.levi714@gmail.com>
2024-10-15 23:11:30 +0100

    ingress and egress groups expressed

--- main.tf
+++ main.tf
@@ -1,11 +1,49 @@
 provider "aws" {
-    region = var.region
+  region = var.region
+}
+
+resource "aws_security_group" "canvas_collective_security" {
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["86.175.168.210/32"]
   }
-    
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  egress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+   egress {
+    from_port   = 53
+    to_port     = 53
+    protocol    = "udp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  tags = {
+    Name = var.aws_security_group_name
+  }
+}
 resource "aws_instance" "canvas_collective" {
-    ami = var.ami_id
-    instance_type = var.instance_type
-    tags = {
-      Name = "canvas_collective"
-    }
-    }
+  ami           = var.ami_id
+  instance_type = var.instance_type
+  key_name      = "canvas_collective"
+  tags = {
+    Name = var.aws_instance
+  }
+}
+
