0327eccff07642a412ebb2012225802e983214d2
Itziar Cantero <itziar.cantero26@gmail.com>
2024-10-16 16:25:27 +0100

    Added imaages to the artists json file

--- src/scripts/modify-database.ts
+++ src/scripts/modify-database.ts
@@ -3,12 +3,38 @@ import DatabaseConstructor, { Database } from "better-sqlite3";
 const fs = require("fs");
 const db: Database = new DatabaseConstructor("db.sqlite3");
 
-const insertData = db.prepare(
-  "INSERT INTO artists (name, bio, socials) VALUES (?, ?, ?)"
+interface Artist {
+    name: string;
+    bio: string;
+    socials: string;
+    image: string;
+}
+
+fs.readFile('../../data/artists.json', 'utf8', (err, data)) => {
+    if (err) {
+        console.error('Error reading JSON file:', err);
+        return;
+    }
+const artists: Artist[] = JSON.parse(data);
+
+
+const insertArtistsData = db.prepare(
+  "INSERT INTO artists (name, bio, socials, image) VALUES (?, ?, ?)"
 );
 
-data.forEach((artist) => {
-  insertData.run(artist.name, artist.bio, artist.socials);
+artists.forEach((artist) => {
+  insertArtistsData.run(artist.name, artist.bio, artist.socials, artist.image, (insertErr) => {
+    if (insertErr) {
+        console.error('Error inserting artist:', insertErr);
+    });
 });
 
 db.close();
+db.close(err => {
+    if (err) {
+        console.error('Error closing the database:', err);
+    } else {
+        console.log('Database closed successfully.');
+    }
+});
+});
