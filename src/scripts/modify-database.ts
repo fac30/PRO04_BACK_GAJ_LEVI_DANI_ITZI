6d7839512bc738476f14b72073d263496d1d01cb
Itziar Cantero <itziar.cantero26@gmail.com>
2024-10-16 15:04:01 +0100

    Created json file with dummie data for Artists

--- src/scripts/modify-database.ts
+++ src/scripts/modify-database.ts
@@ -2,10 +2,6 @@ import DatabaseConstructor, { Database } from "better-sqlite3";
 
 const db: Database = new DatabaseConstructor("db.sqlite3");
 
-const data = [
-    {name: "Gaj", bio: "fghjk", socials: "dfghjuik"},
-    {name: "itzi"}
-]
 
 const insertData = db.prepare("INSERT INTO artists (name, bio, socials) VALUES (?, ?, ?)");
 
