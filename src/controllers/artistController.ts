b3081d3b21f6c3129506531d1dc5c982091e46ca
Gaajia <m.gajia1998@gmail.com>
2024-10-17 11:31:14 +0100

    whatever changes

--- src/controllers/artistController.ts
+++ src/controllers/artistController.ts
@@ -1,14 +1,14 @@
 import { Request, Response } from 'express';
 import { db } from '../scripts/create-database';
 
-export function getAllArtists(req: Request, res: Response) {
+export const getAllArtists = (req: Request, res: Response) => {
 	//get all the endpoints from
 	const stmt = db.prepare('SELECT name FROM artists');
 	const artists = stmt.all();
 	res.json(artists);
-}
+};
 
-export function getArtistById(req: Request, res: Response) {
+export const getArtistById = (req: Request, res: Response) => {
 	//get endpoints for artist with particular id:
 	const { id } = req.params;
 	const stmt = db.prepare('SELECT name FROM artists WHERE id = ?');
@@ -18,4 +18,4 @@ export function getArtistById(req: Request, res: Response) {
 	} else {
 		res.status(404).json({ message: 'Artist not found' });
 	}
-}
+};
