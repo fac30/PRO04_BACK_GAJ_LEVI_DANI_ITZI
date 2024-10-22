a54937bbd8dd7ee6fec89c9e3f8b26e1d45ef511
rogers7levi <rogers.levi714@gmail.com>
2024-10-22 15:57:06 +0100

    changes to CORS config

--- src/index.ts
+++ src/index.ts
@@ -5,8 +5,8 @@ import cors from "cors";
 
 
 import artistRoutes from "./routes/artistRoutes";
-import productRoutes from "./routes/productRoutes"
-import categoriesRoutes from './routes/categoriesRoutes'
+import productRoutes from "./routes/productRoutes";
+import categoriesRoutes from "./routes/categoriesRoutes";
 
 
 dotenv.config();
@@ -14,22 +14,20 @@ dotenv.config();
 const app: Express = express();
 const port = process.env.PORT || 3000;
 
+app.use(cors({
+  origin: 'http://practice.com.s3-website.eu-west-2.amazonaws.com', // Replace with your S3 bucket URL
+  methods: 'GET,POST,PUT,DELETE,OPTIONS',
+  allowedHeaders: 'Content-Type,Authorization'
+}));
+
 app.get('/', (req: Request, res: Response) => {
 	res.send('Canvas Collective');
 });
 
-app.listen(port, () => {
-	console.log(`[server]: Server is running at http://localhost:${port}`);
-});
-
-
-
-app.use(cors({
-  origin: 'http://canvas-collective.s3-website.eu-west-2.amazonaws.com', // Replace with your S3 bucket URL
-  methods: 'GET,POST,PUT,DELETE,OPTIONS',
-  allowedHeaders: 'Content-Type,Authorization'
-}));
- 
 app.use("/", artistRoutes);
 app.use("/", productRoutes)
 app.use('/', categoriesRoutes);
+
+app.listen(port, () => {
+	console.log(`[server]: Server is running at http://localhost:${port}`);
+});
