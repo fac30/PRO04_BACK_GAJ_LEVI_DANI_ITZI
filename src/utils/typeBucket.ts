51c024c4f0fe56c504e1b0998dbd460d1e145d2a
Itziar Cantero <itziar.cantero26@gmail.com>
2024-10-22 11:29:41 +0100

    Add types in the utils folder, type bucket file

--- src/utils/typeBucket.ts
+++ src/utils/typeBucket.ts
@@ -1,1 +1,40 @@
-//Type OR Interface Exported From here.
+export default interface Artist {
+  name: string;
+  bio: string;
+  socials: string;
+  image: string;
+}
+
+export default interface Category {
+  name: string;
+  description: string;
+}
+
+export default interface Product {
+  name: string;
+  description: string;
+  artist_id: number;
+  category_id: number;
+}
+
+export default interface ProductVariation {
+  product_id: number;
+  size: string;
+  colour: string;
+  price: number;
+  stock: number;
+}
+
+export default interface ProductImage {
+  product_id: number;
+  image_url: string;
+  is_main_image: number;
+}
+
+export default interface User {
+  username: string;
+  email: string;
+  hashed_password: string;
+  salt: string;
+  address: string;
+}
