be60c672b1f28d295259798858de3b8b2d8837f8
Danielle <daniellelee73@gmail.com>
2024-10-23 17:21:50 +0100

    fixed the database selection to an all selector. This has now allowed for the endpoints to be corrected and recognised on the front end.

--- src/controllers/artistController.ts
+++ src/controllers/artistController.ts
@@ -1,21 +1,21 @@
-import { Request, Response } from 'express';
-import { db } from '../scripts/create-database';
+import { Request, Response } from "express";
+import { db } from "../scripts/create-database";
 
 export const getAllArtists = (req: Request, res: Response) => {
-	//get all the endpoints from
-	const stmt = db.prepare('SELECT name FROM artists');
-	const artists = stmt.all();
-	res.json(artists);
+  //get all the endpoints from
+  const stmt = db.prepare("SELECT name, id FROM artists");
+  const artists = stmt.all();
+  res.json(artists);
 };
 
 export const getArtistById = (req: Request, res: Response) => {
-	//get endpoints for artist with particular id:
-	const { id } = req.params;
-	const stmt = db.prepare('SELECT name FROM artists WHERE id = ?');
-	const artist = stmt.get(id);
-	if (artist) {
-		res.json(artist);
-	} else {
-		res.status(404).json({ message: 'Artist not found' });
-	}
+  //get endpoints for artist with particular id:
+  const { id } = req.params;
+  const stmt = db.prepare("SELECT * FROM artists WHERE id = ?");
+  const artist = stmt.get(id);
+  if (artist) {
+    res.json(artist);
+  } else {
+    res.status(404).json({ message: "Artist not found" });
+  }
 };
--- src/index.ts
+++ src/index.ts
@@ -3,31 +3,37 @@ import express, { Express, Request, Response } from "express";
 import dotenv from "dotenv";
 import cors from "cors";
 
-
 import artistRoutes from "./routes/artistRoutes";
 import productRoutes from "./routes/productRoutes";
 import categoriesRoutes from "./routes/categoriesRoutes";
-
 
 dotenv.config();
 
 const app: Express = express();
 const port = process.env.PORT || 3000;
 
-app.use(cors({
-  origin: 'http://practice.com.s3-website.eu-west-2.amazonaws.com', // Replace with your S3 bucket URL
-  methods: 'GET,POST,PUT,DELETE,OPTIONS',
-  allowedHeaders: 'Content-Type,Authorization'
-}));
+app.use(
+  cors({
+    origin: "*", // You can replace '*' with your specific S3 bucket URL
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  })
+);
 
-app.get('/', (req: Request, res: Response) => {
-	res.send('Canvas Collective');
+// app.use(cors({
+//   origin: ['*'], // Replace with your S3 bucket URL
+//   methods: 'GET,POST,PUT,DELETE,OPTIONS',
+//   allowedHeaders: 'Content-Type,Authorization'
+// }));
+
+app.get("/", (req: Request, res: Response) => {
+  res.send("Canvas Collective");
 });
 
 app.use("/", artistRoutes);
-app.use("/", productRoutes)
-app.use('/', categoriesRoutes);
+app.use("/", productRoutes);
+app.use("/", categoriesRoutes);
 
 app.listen(port, () => {
-	console.log(`[server]: Server is running at http://localhost:${port}`);
+  console.log(`[server]: Server is running at http://localhost:${port}`);
 });
