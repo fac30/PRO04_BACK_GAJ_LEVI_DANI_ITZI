c981baa746c7bdd565375cd992c856d6d2fc9362
rogers7levi <rogers.levi714@gmail.com>
2024-10-24 12:53:25 +0100

    testing

--- main.tf
+++ main.tf
@@ -40,6 +40,13 @@ resource "aws_security_group" "canvas_collective_security" {
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]  
+  }
   egress {
     from_port   = 0
     to_port     = 0
